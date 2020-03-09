1b8d1be7277245bc2c86681f5c1acfd93537da60
Danie Conradie <danie@curiousmongoose.com>
2020-03-09 22:41:24 +0200

    checkBandMasks break connection. Removed temp

--- Sodaq_Universal_Tracker/Sodaq_R4X.cpp
+++ Sodaq_Universal_Tracker/Sodaq_R4X.cpp
@@ -226,12 +226,9 @@ bool Sodaq_R4X::connect(const char* apn, const char* uratSelect, uint8_t mnoProf
         return false;
     }
 
-    if (uratSelect != SODAQ_R4X_2G_URAT) {
-        if (!checkBandMasks(bandMaskLTE, bandMaskNB)) {
-            return false;
-        }
-    }
-    
+    // if (!checkBandMasks(bandMaskLTE, bandMaskNB)) {
+    //     return false;
+    // }
 
     if (!checkCOPS(operatorSelect != 0 ? operatorSelect : AUTOMATIC_OPERATOR, uratSelect != 0 ? uratSelect : DEFAULT_URAT)) {
         return false;
